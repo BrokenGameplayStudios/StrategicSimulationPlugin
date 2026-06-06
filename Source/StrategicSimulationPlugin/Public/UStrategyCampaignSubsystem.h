0c8d2aa3afb5d22d013f1e045347c5cfdd31cb7e
Inph1del <Inph1del@yahoo.com>
2026-06-05 21:33:13 -0400

    POW KIA Updates

--- Source/StrategicSimulationPlugin/Public/UStrategyCampaignSubsystem.h
+++ Source/StrategicSimulationPlugin/Public/UStrategyCampaignSubsystem.h
@@ -56,13 +56,6 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "POW/KIA|Settings")
     float EnemyKIAChanceOnDefeat = 0.40f;        // when you lose, chance your troops become KIA
 
-    // === KIA SETTINGS (Phase 3) ===
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "POW/KIA|Settings")
-    float KIAChanceOnVictory = 0.25f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "POW/KIA|Settings")
-    float EnemyKIAChanceOnDefeat = 0.40f;
-
     // Debug / testing
     UFUNCTION(BlueprintCallable, Category = "POW/KIA|Debug")
     void SetPOWChance(float NewCaptureChance, float NewKIAChance);
